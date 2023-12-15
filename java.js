4fa07f735a9f921ee572a6679b77e4103b5755bd
Abdoul-Akim <silueakim2007@gmail.com>
2023-12-15 22:17:24 +0000

    graphic

--- java.js
+++ java.js
@@ -1,69 +1,92 @@
+function basic(){
+    const body = document.querySelector('body')
 
-const option = ['pierre','papier','ciseaux'];
+    const textResult = document.createElement('div')
+    textResult.classList.add('textResult')
+    textResult.innerText = 'hello '
 
-function getRandomElement(option) {
-    const RandomIndex = Math.floor(Math.random()*option.length)
-    return option[RandomIndex]
+    const imgResult = document.createElement('div')
+    imgResult.classList.add('imgResult')
+
+    const imgcomputer = document.createElement('img')
+    imgcomputer.classList.add('imgcomputer')
+    imgcomputer.src = './none.png'
+
+    const imgUser = document.createElement('img')
+    imgUser.classList.add('imgUser')
+    imgUser.src = './none.png'
+
+    imgResult.appendChild(imgUser)
+    imgResult.appendChild(imgcomputer)
+
+    body.appendChild(textResult)
+    body.appendChild(imgResult)
+
+}
+
+function button(object){
+    const body = document.querySelector('body')
+
+    const buttonContainer = document.createElement('div')
+    buttonContainer.classList.add('button')
+
+    Object.values(object).forEach(function(item){
+        const button = document.createElement('button')
+        button.onclick = function(){
+            PlayRound(item, getRandomElement(object))}
+        const img = document.createElement('img')
+        img.src = item.img
+        button.appendChild(img)
+        buttonContainer.appendChild(button)
+    })
+    body.appendChild(buttonContainer)
 }
 
 
 
-function PlayRound(UserEntry,ComputerChoice){
-    
-    if (UserEntry =='pierre' && ComputerChoice =='ciseaux'){
-        return 'La pierre casse le ciseaux. Vous avez gagnez.'
-    }
-    else if (UserEntry =='papier' && ComputerChoice =='pierre'){
-        return 'Le papier embale la pierre. Vous avez gagnez.'
-    }
-    else if (UserEntry =='ciseaux' && ComputerChoice =='papier'){
-        return 'les ciseaux coupe le papier. Vous avez gagnez.'
-    }
-    else if (ComputerChoice =='ciseaux' && UserEntry =='papier'){
-        return 'les ciseaux coupe le papier. Vous avez perdue.'
-    }
-    else if (ComputerChoice =='papier' && UserEntry =='pierre'){
-        return 'Le papier embale la pierre. Vous avez perdue.'
-    }
-    else if (ComputerChoice =='pierre' && UserEntry =='ciseaux'){
-        return 'La pierre casse le ciseaux. Vous avez perdue.'
-    }
-    else{
-        return ('egalite')
-    }
-}
-function Game(){
-    v = 0
-    d = 0
-
-for(let i = 0; i<5; i++){
-
-    const ComputerChoice = getRandomElement(option).toLowerCase()
-
-    console.log(ComputerChoice)
-
-    const UserEntry = prompt(`pierre,papier,ciseaux`).toLowerCase()
-
-    let result = PlayRound(UserEntry,ComputerChoice)
-
-    console.log(result)
-
-    if(result.includes('perdue')){
-        v += 1
-    }
-    else if(result.includes('perdue')){
-        d += 1
-    }
-}
-if(d>v){
-    console.log(`vous avez perdue a ${d} contre ${v}`)
-}
-else if(d<v){
-    console.log(`vous avez gagnez a ${v} contre ${d}`)
-}
-else{
-    console.log(`il y a egalite a ${v} a ${d}`)
-}
+function getRandomElement() {
+    const randomIndex = Math.floor(Math.random() * 3);
+    const choice = option[randomIndex+1];
+    return choice;
 }
 
-Game()
+
+function PlayRound(UserEntry, ComputerChoice) {
+    const option = {
+        'pierre-ciseaux': 'La pierre casse le ciseaux. Vous avez gagné.',
+        'papier-pierre': 'Le papier emballe la pierre. Vous avez gagné.',
+        'ciseaux-papier': 'Les ciseaux coupent le papier. Vous avez gagné.',
+        'papier-ciseaux': 'Les ciseaux coupent le papier. Vous avez perdu.',
+        'pierre-papier': 'Le papier emballe la pierre. Vous avez perdu.',
+        'ciseaux-pierre': 'La pierre casse le ciseaux. Vous avez perdu.',
+        'ciseaux-ciseaux': 'Égalité.',
+        'pierre-pierre': 'Égalité.',
+        'papier-papier': 'Égalité.'
+    }
+
+    const result = option[`${UserEntry.name}-${ComputerChoice.name}`];
+    const textResult = document.querySelector('.textResult')
+    const imgUser = document.querySelector('.imgUser')
+    const imgcomputer = document.querySelector('.imgcomputer')
+    textResult.innerText = result
+    imgUser.src = UserEntry.img
+    imgcomputer.src = ComputerChoice.img
+}
+
+const option = {
+    1:{
+        name:'pierre',
+        img:'./pierre.png'
+    },
+    2:{
+        name:'papier',
+        img:'./papier.png'
+    },
+    3:{
+        name:'ciseaux',
+        img:'./ciseaux.png'
+    }
+}
+
+basic()
+button(option)
